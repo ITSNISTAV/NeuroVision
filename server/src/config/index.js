dbec485076a218bd61afac2f2eac1fac3f56fdcb
Lakshita Sharma <lakshitadhariwal@gmail.com>
2026-03-02 20:07:04 +0530

    simplified controllers

--- server/src/config/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-const DATA_DIR = path.join(__dirname, '..', '..', 'data')
-const USERS_FILE = path.join(DATA_DIR, 'users.json')
-
-module.exports = {
-  DATA_DIR,
-  USERS_FILE,
-}
